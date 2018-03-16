1a1b23b2388bf181ee28de6c8abc4d852057ba2e
ekosusil <ekosusil@10.0.0.103>
2018-03-16 22:40:20 +0100

    commit forgotten class

---
+++ idealo-booking/app/util/constants.scala
@@ -0,0 +1,9 @@
+package util
+
+object Constants {
+  val USER_NOT_REGISTERED_ERROR = "User not registered"
+  val TRAVEL_CODE_NOT_VALID_ERROR = "Travel code not valid"
+  val NO_SEAT_LEFT_ERROR = "Not enough seat available"
+  val INVALID_SEAT_ARGUMENT_ERROR = "Invalid number of seat specified"
+
+}
